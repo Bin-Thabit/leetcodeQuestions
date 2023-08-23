75b30995a002e2e20502e043bb526d7bad088e81
Khalid <klood_12@hotmail.com>
2023-08-23 23:14:54 +0400

    I modified the answer but it has the same answer which was accepted in leetcode but this anser is better

--- src/leetCode/SearchinRotatedSortedArray.java
+++ src/leetCode/SearchinRotatedSortedArray.java
@@ -1,48 +1,52 @@
 package leetCode;
 
 public class SearchinRotatedSortedArray {
-	// https://leetcode.com/problems/search-in-rotated-sorted-array/description/
-	// 33. Search in Rotated Sorted Array
-	public int search(int[] nums, int target) {
-        int pivot = findPivot(nums);
-        int firstHalf = binarySearch(nums , target , 0 , pivot);
-        int secondHalf = binarySearch(nums , target , pivot+1 , nums.length - 1);
-        if(firstHalf == -1)
-            return secondHalf;
-        else
-            return firstHalf;  
-    }
-	// Here this method returned the pivot
-	// pivot: the largest element in the array
-    public int findPivot(int [] nums){
-        int start = 0;
-        int end = nums.length - 1;
-        while(start < end){
-            int mid = start + (end - start) / 2;
-            if(mid < end && nums[mid] > nums[mid + 1])
-                return mid;
-            else if(mid > start && nums[mid] < nums[mid - 1])
-                return mid - 1;
-            else if(nums[mid] <= nums[start])
-                end = mid - 1;
-            else
-                start = mid + 1;                    
-        }
-        return end;
-    }
-    // Normal binary search
-    int binarySearch(int [] nums, int target , int s, int e){
-        int start = s;
-        int end = e;
-        while(start <= end){
-            int mid = start + (end - start) / 2;
-            if(target == nums[mid])
-                return mid;
-            else if (target < nums[mid])
-                end = mid - 1;
-            else
-                start = mid + 1;        
-        }
-        return -1;
-    }
+	 // https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+		// 33. Search in Rotated Sorted Array
+	    static int search(int[] array, int target) {
+			int pivot =  findPivot(array);
+			if(pivot == -1)
+				return binarySearch(array, target, 0 , array.length - 1 );
+			else if(array[pivot] == target)
+				return pivot;
+			else if(array[0] > target)
+				return binarySearch(array, target, pivot + 1 , array.length - 1 );
+			else
+				return binarySearch(array, target, 0 ,pivot );
+		}
+		// Here this method returned the pivot
+		// pivot: the largest element in the array
+		static int findPivot(int []array) {
+			int start = 0;
+			int end = array.length - 1;
+			while(start <= end) {
+				int mid = start + (end - start) / 2;
+				if(mid < end && array[mid] > array[mid + 1])
+					return mid;
+				else if(mid > start && array[mid] < array[mid - 1])
+					return mid - 1;
+				else if(array[start] >= array[mid])
+					end = mid - 1;
+				else
+					start = mid + 1;
+			}
+			return -1;
+		}
+		// Binary search
+		static int binarySearch(int [] nums, int target , int s, int e) {
+			int start = s;
+			int end = e;
+			
+			while(start <= end) {
+				int mid = start + (end - start) /2;
+				if(nums[mid] == target)
+					return mid;
+				else if(target < nums[mid])
+					end = mid - 1;
+				else
+					start = mid + 1; 
+			}
+			// return -1 if it doesn't exist
+			return -1;
+		}
 }
