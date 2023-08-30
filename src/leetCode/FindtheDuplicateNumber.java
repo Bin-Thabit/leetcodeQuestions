1ed5976935498be4b3418eb15aa6c7c497dc1c54
Khalid <klood_12@hotmail.com>
2023-08-30 23:08:15 +0400

    I solved FindAllDuplicatesinanArray question from leetcode

--- src/leetCode/FindtheDuplicateNumber.java
+++ src/leetCode/FindtheDuplicateNumber.java
@@ -6,6 +6,7 @@ public class FindtheDuplicateNumber {
 	public int findDuplicate(int[] nums) {
         int i = 0;
         while(i < nums.length){
+        	
             if(nums[i] != i+1){
             int correct = nums[i] - 1;
             if(nums[i] != nums[correct])
