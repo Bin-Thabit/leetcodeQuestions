ee55309a6a972573d482d07139e792561dda8066
Khalid <klood_12@hotmail.com>
2023-09-05 23:05:24 +0400

    I solved Defanging an IP Address question from leetcode

--- src/StringQuestions/DefanginganIPAddress.java
+++ src/StringQuestions/DefanginganIPAddress.java
@@ -1,5 +1,9 @@
 package StringQuestions;
 
 public class DefanginganIPAddress {
-
+	// https://leetcode.com/problems/defanging-an-ip-address/description/
+	// 1108. Defanging an IP Address
+	 public String defangIPaddr(String address) {
+	       return address.replace(".","[.]");
+	    }
 }
