cf22382f45b004b54e3a0908e4ad079884c1e6f6
Khalid <klood_12@hotmail.com>
2023-09-11 15:04:21 +0400

    I added another solution for Reverse Bits question from leetcode

--- src/bitWiseQuestions/ReverseBits.java
+++ src/bitWiseQuestions/ReverseBits.java
@@ -5,6 +5,7 @@ public class ReverseBits {
 	// https://leetcode.com/problems/reverse-bits/description/
 	// 190. Reverse Bits
 
+	// solution 1
     public int reverseBits(int n) {
         int count = 0;
         int num = 0;
@@ -18,4 +19,22 @@ public class ReverseBits {
         }
         return num;
     }
+    
+    // solution 2
+    /*
+    public int reverseBits(int n) {
+        int f = 31;
+        int l = 0;
+        int reverse = 0; 
+        while(f > l){
+            if((n & (1 << f)) != 0)
+                 reverse = reverse | (1<<l);
+            if((n & (1 << l)) != 0)
+                 reverse = reverse | (1<<f);
+            f--;
+            l++;          
+        }
+        return reverse;
+     }
+     */
 }
