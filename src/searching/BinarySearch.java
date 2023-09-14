9c604549e84c51c469a99715ebd9ce3fc0ec7211
Khalid <klood_12@hotmail.com>
2023-09-15 00:25:06 +0400

    I added another solution to the Binary Search question using recursion

--- src/searching/BinarySearch.java
+++ src/searching/BinarySearch.java
@@ -18,5 +18,25 @@ public class BinarySearch {
         }
         return -1;
     }
+	
+	// Using recursion
+	/*
+	 public int search(int[] nums, int target) {
+	       int ans = binarySearch(nums, target, 0, nums.length - 1);
+	       return ans;
+	    }
+
+	    static int binarySearch(int[] arr, int target, int start, int end){
+	        if(start > end)
+	            return -1;
+	        int mid = start + (end - start) / 2;
+	        if(arr[mid] == target)
+	            return mid;
+	        else if(target < arr[mid])
+	            return binarySearch(arr, target, start, mid - 1);
+	        else
+	            return binarySearch(arr, target, mid + 1, end);            
+	    }
+	 */
 
 }
