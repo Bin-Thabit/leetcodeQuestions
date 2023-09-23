b971642cedf556ac437e6a1f7718466e3eaafc12
Khalid <klood_12@hotmail.com>
2023-09-23 21:27:22 +0400

    I added another solution on Intersection of Two Arrays question from leetcode

--- src/searching/IntersectionofTwoArrays.java
+++ src/searching/IntersectionofTwoArrays.java
@@ -7,6 +7,7 @@ public class IntersectionofTwoArrays {
 	// https://leetcode.com/problems/intersection-of-two-arrays/description/
 	// 349. Intersection of Two Arrays
 	
+	// Solution 1
 	public int[] intersection(int[] nums1, int[] nums2) {
         HashSet<Integer> set1 = new HashSet<Integer>();
         createSet(set1,nums1);
@@ -27,4 +28,38 @@ public class IntersectionofTwoArrays {
         for(int num : array)
             set.add(num);
     }
+    
+    // Solution 2
+    /*
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int[] temp = new int[1001];
+        // Thus counter to know the size of the array
+        int counter = 0;
+        for(int i = 0; i < nums1.length; i++){
+            // Here every elements you found temp[nums1[i]] = 1
+            temp[nums1[i]] = 1;
+        }
+
+        for(int i = 0; i < nums2.length; i++){
+            // Here every elements you found which is exist in nums1 temp[nums2[i]] = 2
+            // and add the counter by 1
+           if(temp[nums2[i]] == 1){
+               temp[nums2[i]] = 2;
+               counter++; 
+           }
+        }
+        
+        int[] ans = new int[counter];
+        for(int i = 0 , j = 0; i < temp.length && j < counter; i++){
+            // If you found any element = 2, then add it to the ans array
+            if(temp[i] == 2){
+                ans[j] = i;
+                j++;
+            }
+        }
+
+        return ans;
+
+    }
+    */
 }
