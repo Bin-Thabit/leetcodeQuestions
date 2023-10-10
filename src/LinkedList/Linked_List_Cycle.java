809dc757661062dd11c5fcbf99b81b016c017311
Khalid <klood_12@hotmail.com>
2023-10-10 16:34:36 +0400

    I created LinkedList/Linked_List_Cycle.class, i solved Linked List Cycle question from leetcode

---
+++ src/LinkedList/Linked_List_Cycle.java
@@ -0,0 +1,22 @@
+package LinkedList;
+
+public class Linked_List_Cycle {
+
+	// https://leetcode.com/problems/linked-list-cycle/description/
+	// 141. Linked List Cycle
+
+	/*
+	 public boolean hasCycle(ListNode head) {
+	        ListNode fast = head;
+	        ListNode slow = head;
+	        while(fast != null && fast.next != null){
+	            fast = fast.next.next;
+	            slow = slow.next; 
+	            if(fast == slow){
+	                return true;
+	            }
+	        }
+	        return false;
+	    }
+	    */
+}
