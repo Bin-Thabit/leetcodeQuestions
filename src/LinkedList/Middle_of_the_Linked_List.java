bacf463829d0669a5fbfd8808cd0ebee2de2cdfc
Khalid <klood_12@hotmail.com>
2023-10-13 20:51:17 +0400

    I created LinkedList/Middle_of_the_Linked_List.class, i solved Middle of the Linked List question from leetcode

---
+++ src/LinkedList/Middle_of_the_Linked_List.java
@@ -0,0 +1,21 @@
+package LinkedList;
+
+public class Middle_of_the_Linked_List {
+
+	// https://leetcode.com/problems/middle-of-the-linked-list/description/
+	// 876. Middle of the Linked List
+	
+	// Fast and slow approach
+	/*
+	public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    */
+
+}
