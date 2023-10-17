026401181c3ca11b43bb0b4f6051f627de23d250
Khalid <klood_12@hotmail.com>
2023-10-17 10:36:37 +0400

    I created LinkedList/Convert_Binary_Number_in_a_Linked_List_to_Integer.class, i solved this question from leetcode

---
+++ src/LinkedList/Convert_Binary_Number_in_a_Linked_List_to_Integer.java
@@ -0,0 +1,5 @@
+package LinkedList;
+
+public class Convert_Binary_Number_in_a_Linked_List_to_Integer {
+
+}
--- src/LinkedList/Reverse_Linked_List.java
+++ src/LinkedList/Reverse_Linked_List.java
@@ -2,4 +2,21 @@ package LinkedList;
 
 public class Reverse_Linked_List {
 
+	// https://leetcode.com/problems/reverse-linked-list/description/
+	// 206. Reverse Linked List
+
+	/*
+	public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next;
+        while(current != null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next; 
+        }
+        return prev;    
+    }
+    */
 }
